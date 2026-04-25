0cb141bb739be5c0172cc836522b0b78ec290144
Faisal-ops <faisansari59@gmail.com>
2026-04-25 16:00:17 +0530

    Bump version to 1.2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.zeno.classiclauncher.nlauncher"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.2.4"
+        versionCode = 9
+        versionName = "1.2.5"
     }
 
     buildFeatures {
