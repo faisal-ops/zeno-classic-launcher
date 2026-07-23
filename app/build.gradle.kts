4f394bb5a2d71d3f5089f3af58af28da85494064
Faisal-ops <faisansari59@gmail.com>
2026-07-23 19:40:59 +0530

    Bump version to 1.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.zeno.classiclauncher.nlauncher"
         minSdk = 26
         targetSdk = 35
-        versionCode = 28
-        versionName = "1.6.0"
+        versionCode = 29
+        versionName = "1.6.1"
     }
 
     buildFeatures {
